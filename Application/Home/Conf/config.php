a203d0a083153b009706c2e2223909dbbfd50a01
Chao Bin <chaobin.sncer@gmail.com>
2016-05-24 23:20:46 +0800

    新增注册页面

--- Application/Home/Conf/config.php
+++ Application/Home/Conf/config.php
@@ -3,7 +3,8 @@ return array(
 	//'配置项'=>'配置值'
 	
 	'TMPL_PARSE_STRING' => array(
-        '__STATIC__' => __ROOT__.'/Application/'.MODULE_NAME.'/View/' . '/Public/static'
+        '__STATIC__' => __ROOT__.'/Application/'.MODULE_NAME.'/View' . '/Public/static',
+        '__DOCUMENTS__' => __ROOT__.'/Application/'.MODULE_NAME.'/View' . '/Public/documents'
         
     ),
 );
