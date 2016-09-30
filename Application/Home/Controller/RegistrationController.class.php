0ee50aa3fbbeddfe71b487b155127e56ee7aa018
Chao Bin <chaobin.sncer@gmail.com>
2016-09-30 19:56:39 +0800

    新增用户查看注册信息页面 新增用户查看注册详情功能 新增用户删除注册信息功能（更改状态为0，不是真删除） 新增用户代注册页面

--- Application/Home/Controller/RegistrationController.class.php
+++ Application/Home/Controller/RegistrationController.class.php
@@ -18,8 +18,8 @@ class RegistrationController extends CommonController {
 	//代别人注册页面，填写个人信息，写入reg表
 	public function attendee_reg(){
 		//获取操作流程，默认为reg_other
-//		$action = $_REQUEST['action'];
-//		$this->assign("action",$action);
+		$action = $_REQUEST['action'];
+		$this->assign("action",$action);
 		
 		$this->assign("title_list",C('TITLE_LIST'));
 		$this->assign("country_list",C('COUNTRY_LIST'));
@@ -161,7 +161,7 @@ class RegistrationController extends CommonController {
 			}
 			$Reg->commit();
 			$curr_time = time();
-			$early_time = strtotime (date("2016-11-01")); //Early Bird 截止期日
+			$early_time = strtotime (date("2016-11-01")); //Early Bird 截止期日,下面还有一个
 			$refer_type_list = C('REFER_TYPE_LIST');
 			$payment_url_list = C('PAYMENT_URL_LIST');
 			$total_cost_list = C('TOTAL_COST_LIST');
@@ -287,8 +287,212 @@ class RegistrationController extends CommonController {
 		
 		$this->display();
 	}
+	//打开注册管理页面
+	public function manage_reg(){
+		
+		//如果没有登录则报错
+		if(!session('?user')){
+			$this->display('Public:500');
+			exit;
+		}
+		
+		$user = session('user');
+		$user_id = $user['user_id'];
+		
+		//查询该用户提交的摘要
+		$Reg = M('Reg');
+		
+		$regs = $Reg->order("created_time")->where("user_id = $user_id and status = 1")->select();
+		
+		$refer_type_list = C('REFER_TYPE_LIST');
+		
+		$this->assign('regs',$regs);
+		$this->assign('user',$user);
+		$this->assign('refer_type_list',$refer_type_list);
+		$this->display();
+	}
+	//删除注册，将status由1更新成0
+	public function delete_reg(){
+		$reg_id = $_POST['reg_id'];
+		if(!isset($reg_id)){
+			$this->display('Public:500');
+			exit;
+		}
+		//如果没有登录则报错
+		if(!session('?user')){
+			$this->display('Public:500');
+			exit;
+		}
+		$user = session('user');
+		$user_id = $user['user_id'];
+		
+		$Reg = M('Reg');
+		$data['status'] = 0;
+		$data['updated_time'] = date("Y-m-d H:i:s");
+		// 根据条件更新记录
+		$result = $Reg->where("user_id =$user_id and reg_id=$reg_id")->save($data); 
+		if($result){
+			//删除成功
+			echo json_encode(array(
+	    		'result' => "success",
+			));
+		}else{
+			echo json_encode(array(
+	    		'result' => "fail",
+			));
+		}
+	}
 	
-	
+	public function details_reg(){
+		$reg_id = $_REQUEST['reg_id'];
+		if(!isset($reg_id)){
+			$this->display('Public:500');
+			exit;
+		}
+		//如果没有登录则报错
+		if(!session('?user')){
+			$this->display('Public:500');
+			exit;
+		}
+		$user = session('user');
+		$user_id = $user['user_id'];
+		$Reg = M('Reg');
+		//查询该用户的注册单
+		$data = $Reg->where("user_id =$user_id and reg_id=$reg_id")->find();
+		if(!$data){
+			$this->display('Public:500');
+			exit;
+		}
+		
+		//如果需要邀请函,查询visa信息
+		if($data['is_visa'] == 1){
+			$Visa = M("Visa");
+			$visa = $Visa->where("user_id =$user_id and reg_id=$reg_id")->find();
+			if(!$visa){
+				$this->display('Public:500');
+				exit;
+			}
+		}
+		
+		$curr_time = time();
+		$early_time = strtotime (date("2016-11-01")); //Early Bird 截止期日,上面还有一个
+		$refer_type_list = C('REFER_TYPE_LIST');
+		$payment_url_list = C('PAYMENT_URL_LIST');
+		$total_cost_list = C('TOTAL_COST_LIST');
+		
+		//如果是信用卡支付
+		if ($data['pay_type'] == 1) {
+			//如果在截止日期之前
+			if($curr_time < $early_time){
+				//根据类型判断价格和支付链接
+				switch ($data['refer_type'])
+				{
+					case 1:
+						$payment_url = $payment_url_list["1"];
+						$total_cost = $total_cost_list["1"];
+						break;
+					case 2:
+						$payment_url = $payment_url_list["3"];
+						$total_cost = $total_cost_list["3"];
+						break;
+					case 3:
+						$payment_url = $payment_url_list["5"];
+						$total_cost = $total_cost_list["5"];
+						break;
+					case 4:
+						$payment_url = $payment_url_list["7"];
+						$total_cost = $total_cost_list["7"];
+						break;
+					default:
+						$payment_url = "";
+						$total_cost = 0;
+				}
+				
+			}else{
+				switch ($data['refer_type'])
+				{
+					case 1:
+						$payment_url = $payment_url_list["2"];
+						$total_cost = $total_cost_list["2"];
+						break;
+					case 2:
+						$payment_url = $payment_url_list["4"];
+						$total_cost = $total_cost_list["4"];
+						break;
+					case 3:
+						$payment_url = $payment_url_list["6"];
+						$total_cost = $total_cost_list["6"];
+						break;
+					case 4:
+						$payment_url = $payment_url_list["8"];
+						$total_cost = $total_cost_list["8"];
+						break;
+					default:
+						$payment_url = "";
+						$total_cost = 0;
+				}
+			}
+			$this->assign("payment_url",$payment_url);
+			
+		} else {
+			//如果是转账
+			//如果在截止日期之前
+			if($curr_time < $early_time){
+				//根据类型判断价格
+				switch ($data['refer_type'])
+				{
+					case 1:
+						$total_cost = $total_cost_list["1"];
+						break;
+					case 2:
+						$total_cost = $total_cost_list["3"];
+						break;
+					case 3:
+						$total_cost = $total_cost_list["5"];
+						break;
+					case 4:
+						$total_cost = $total_cost_list["7"];
+						break;
+					default:
+						$total_cost = 0;
+				}
+				
+			}else{
+				switch ($data['refer_type'])
+				{
+					case 1:
+						$total_cost = $total_cost_list["2"];
+						break;
+					case 2:
+						$total_cost = $total_cost_list["4"];
+						break;
+					case 3:
+						$total_cost = $total_cost_list["6"];
+						break;
+					case 4:
+						$total_cost = $total_cost_list["8"];
+						break;
+					default:
+						$total_cost = 0;
+				}
+			}
+			$this->assign("bank_account_details",C("BANK_ACCOUNT_DETAILS"));
+		}
+		$this->assign("pay_type",$data['pay_type']);
+		$this->assign("total_cost",$total_cost);
+		$this->assign("refer_type_list",$refer_type_list);
+		$this->assign("refer_type",$data['refer_type']);
+		$this->assign("refer_no",$data['refer_no']);
+		$this->assign("reg",$data);
+		$this->assign("visa",$visa);
+		$this->display('thanks');
+		
+	}
+
+	public function new_reg(){
+		
+		$this->display();
+	}
 	
 	//发送自动创建账户的电子邮件
 	public function account_mail($toAddress,$subject,$title,$last_name,$password){
