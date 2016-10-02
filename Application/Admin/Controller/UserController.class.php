005cc5f53f9f0412026f358f049fa531328f2472
Chao Bin <chaobin.sncer@gmail.com>
2016-10-02 17:27:11 +0800

    修复注册时间未查询的bug

--- Application/Admin/Controller/UserController.class.php
+++ Application/Admin/Controller/UserController.class.php
@@ -20,7 +20,7 @@ class UserController extends CommonController {
 		$User = M('User');
 		
 		$users = $User->table('icb_user user')->where('user.active > 0')->order('user.created_time desc')->group('user.user_id')
-		->field('user.user_id,user.title,user.first_name,user.last_name,user.email,user.affiliation,user.address,user.zip,user.city,user.country,count(DISTINCT abs.abstract_id) abs_num, count(DISTINCT reg.reg_id) reg_num')
+		->field('user.user_id,user.title,user.first_name,user.last_name,user.email,user.affiliation,user.address,user.zip,user.city,user.country,user.created_time,count(DISTINCT abs.abstract_id) abs_num, count(DISTINCT reg.reg_id) reg_num')
 		->join('left join icb_abstract abs on abs.user_id = user.user_id and abs.status > 0')
 		->join('left join icb_reg reg on reg.user_id = user.user_id and reg.status > 0')
 		->select();
