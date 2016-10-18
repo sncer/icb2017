215a23a84ab7c360edb300e4cf4603df921dd25a
Chao Bin <chaobin.sncer@gmail.com>
2016-10-18 17:28:47 +0800

    可查看注册人员有没有提交摘要 可查看title称呼

--- Application/Admin/Controller/RegistrationController.class.php
+++ Application/Admin/Controller/RegistrationController.class.php
@@ -178,9 +178,11 @@ class RegistrationController extends CommonController {
 		//查询该用户提交的摘要
 		$Reg = M('Reg');
 		
-		$regs = $Reg->table('icb_reg reg')->order("reg.created_time")->where("reg.status = 1 and reg.is_visa = 1")
-		->field('reg.*,visa.full_name,visa.address,visa.zip,visa.city,visa.country,visa.birth_date')
+		$regs = $Reg->table('icb_reg reg')->order("reg.created_time")->where("reg.status = 1")
+		->field('reg.*,visa.full_name,visa.address,visa.zip,visa.city,visa.country,visa.birth_date,count(DISTINCT abs.abstract_id) abs_num')
+		->group('reg.reg_id')
 		->join('left join icb_visa visa on reg.reg_id = visa.reg_id')
+		->join('left join icb_abstract abs on reg.user_id = abs.user_id and concat(reg.first_name,reg.last_name) = (select concat(user.first_name,user.last_name) from icb_user user where user.user_id = reg.user_id )')
 		->select();
 		
 		$refer_type_list = C('REFER_TYPE_LIST');
