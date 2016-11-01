fa73ac2cf82486dd436b9a777bf0e0417a295b3c
Chao Bin <chaobin.sncer@gmail.com>
2016-11-01 11:47:22 +0800

    将early bird时间延迟到11月15号

--- Application/Admin/Controller/RegistrationController.class.php
+++ Application/Admin/Controller/RegistrationController.class.php
@@ -58,7 +58,7 @@ class RegistrationController extends CommonController {
 		}
 		
 		$curr_time = time();
-		$early_time = strtotime (date("2016-11-01")); //Early Bird 截止期日,上面还有一个
+		$early_time = strtotime (date("2016-11-15")); //Early Bird 截止期日
 		$refer_type_list = C('REFER_TYPE_LIST');
 		$payment_url_list = C('PAYMENT_URL_LIST');
 		$total_cost_list = C('TOTAL_COST_LIST');
